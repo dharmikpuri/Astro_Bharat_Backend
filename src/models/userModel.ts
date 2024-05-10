6244cafbb51f62b7312ecb5e94102c52f5089b46
Dharmik Puri Goswami <dharmikpuri2004@gmail.com>
2024-05-10 11:35:46 +0530

    User Model Created

---
+++ src/models/userModel.ts
@@ -0,0 +1,41 @@
+import { Mode } from "fs";
+import mongoose,{Model,Document} from "mongoose"
+
+interface IUser extends Document{
+ name:string,
+ gender:string,
+ email:string,
+ languages:[string],
+ specialities:[string],
+ profileImageUrl:string
+}
+const userSchema = new mongoose.Schema<IUser>({
+    name:{
+        type:String,
+        require:true
+    },
+    gender:{
+        type:String,
+        enum:["male","female","others"],
+        require:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    languages:{
+        type:[String],
+        require:true
+    },
+    specialities:{
+        type:[String],
+        require:true
+    },
+    profileImageUrl:{
+        type:String,
+        require:true
+    }
+})
+
+export const userModel:Model<IUser> = mongoose.model<IUser>("users",userSchema);
